9a74b5fad15c7593cad1210e50a876fcd84d7efb
ahmed elnagger <ahmedalnaggar19955@gmail.com>
2020-04-10 11:01:09 +0200

    Merge branch 'master' of C:\Users\elnaggar\IdeaProjects\ecommerce with conflicts.

--- src/main/java/com/arams/servlets/Test.java
+++ src/main/java/com/arams/servlets/Test.java
@@ -1,12 +1,9 @@
 package com.arams.servlets;
 
 
-import com.arams.beans.Category;
-import com.arams.beans.Product;
 import com.arams.beans.User;
-import com.arams.db.connection.HibernateConnector;
-import com.arams.db.dao.classes.ProductDao;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -26,6 +23,12 @@ public class Test extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-        
+        PrintWriter out = res.getWriter();
+
+        //User user=UserDao.getUser(1);
+        //List<User> allUsers=UserDao.getAllUser();
+        //Gson gson = new Gson();
+        //out.println(gson.toJson(user));
+        //out.println(gson.toJson(allUsers, new TypeToken<List<User>>() {}.getType()));
     }
 }
