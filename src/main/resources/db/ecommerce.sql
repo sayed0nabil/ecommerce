36aca282a916cedb5e1f75e5ba0b00b5a6b4be4f
navras <moamen.zakaria@gmail.com>
2020-04-10 21:10:27 +0200

    database update

--- src/main/resources/db/ecommerce.sql
+++ src/main/resources/db/ecommerce.sql
@@ -5,9 +5,6 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
--- -----------------------------------------------------
 -- Schema ecommerce
 -- -----------------------------------------------------
 
@@ -26,10 +23,11 @@ CREATE TABLE IF NOT EXISTS `ecommerce`.`user` (
   `first_name` VARCHAR(45) NOT NULL,
   `last_name` VARCHAR(45) NOT NULL,
   `email` VARCHAR(45) NOT NULL,
+  `image_name` VARCHAR(45),
   `password` VARCHAR(45) NOT NULL,
   `birth_date` DATE NULL,
-  `image_name` VARCHAR(45),
   `admin` TINYINT NOT NULL,
+  `image_name` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE)
 ENGINE = InnoDB;
@@ -39,9 +37,10 @@ ENGINE = InnoDB;
 -- Table `ecommerce`.`category`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `ecommerce`.`category` (
-  `id` INT NOT NULL,
+  `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`))
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `category_name_UNIQUE` (`name` ASC) VISIBLE)
 ENGINE = InnoDB;
 
 
@@ -53,14 +52,15 @@ CREATE TABLE IF NOT EXISTS `ecommerce`.`product` (
   `name` VARCHAR(45) NOT NULL,
   `price` INT NOT NULL,
   `quantity` INT NOT NULL,
+  `description` VARCHAR(250),
   `category_id` INT NOT NULL,
   PRIMARY KEY (`id`),
   INDEX `category_id_idx` (`category_id` ASC) VISIBLE,
-  CONSTRAINT `category_id`
+  CONSTRAINT `category_id_cons`
     FOREIGN KEY (`category_id`)
     REFERENCES `ecommerce`.`category` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
 
@@ -72,11 +72,11 @@ CREATE TABLE IF NOT EXISTS `ecommerce`.`product_image` (
   `url` VARCHAR(45) NOT NULL,
   `quantity` INT NOT NULL,
   PRIMARY KEY (`product_id`, `url`),
-  CONSTRAINT `product_id`
+  CONSTRAINT `product_id_image_cons`
     FOREIGN KEY (`product_id`)
     REFERENCES `ecommerce`.`product` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
 
@@ -99,19 +99,28 @@ CREATE TABLE IF NOT EXISTS `ecommerce`.`user_product_cart` (
   `product_id` INT NOT NULL,
   `quantity` INT NOT NULL,
   PRIMARY KEY (`user_id`, `product_id`),
-  INDEX `product_id_idx` (`product_id` ASC) VISIBLE,
-  CONSTRAINT `user_id`
+  CONSTRAINT `user_id_cons`
     FOREIGN KEY (`user_id`)
     REFERENCES `ecommerce`.`user` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `product_id2`
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `product_id_car_cons`
     FOREIGN KEY (`product_id`)
     REFERENCES `ecommerce`.`product` (`id`)
     ON DELETE CASCADE
     ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
+insert into ecommerce.user (credit_limit, first_name, last_name, email, password, admin)
+values (1000, 'Admin', 'Admin', 'admin@gmail.com', 'admin', 1);
+insert into ecommerce.user (credit_limit, first_name, last_name, email, password, admin)
+values (1000, 'Elsayed', 'Nabil', 'sayed@gmail.com', 'sayed', 0);
+
+insert into ecommerce.category (id, name)
+values (1, 'Electronics') ;
+
+insert into ecommerce.product (name, price, quantity, description, category_id)
+values('Laptop', 5000, 10, 'Lenovo 15.6 laptop casual backpack b210 black-row.', 1);
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
